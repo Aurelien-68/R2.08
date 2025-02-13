db469a4e482b304a4f9c451b9e34f7009059d601
e2400575 <aurelienhenry9@gmail.com>
2025-02-13 11:20:00 +0100

    f

--- TD1/db_moy.py
+++ TD1/db_moy.py
@@ -1,4 +1,19 @@
 import sys
-import pathlib
+from pathlib import Path
 
 if __name__=='__main__':
+    if len(sys.argv)<5:
+        print(f"il doit y avoir 4 argument après {sys.argv[0]}")
+        print("Utilisation: python dbmoy.py nom_fichier.txt NOM Prenom note1 [note2]...")
+        sys.exit(1)
+
+    fichier=sys.argv[1]
+    f_path= Path(fichier)
+    f_path=f_path.resolve()
+
+
+    nom=sys.argv[2].upper()
+    prenom=sys.argv[3].capitalize()
+    notes=sys.argv[4:]
+    try:
+
