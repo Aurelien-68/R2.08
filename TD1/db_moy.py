4235ee4f7c97a71ced3724a1c295ea055eb7892a
e2400575 <aurelienhenry9@gmail.com>
2025-02-13 11:36:03 +0100

    f

--- TD1/db_moy.py
+++ TD1/db_moy.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 from pathlib import Path
 
@@ -10,10 +11,21 @@ if __name__=='__main__':
     fichier=sys.argv[1]
     f_path= Path(fichier)
     f_path=f_path.resolve()
-
-
     nom=sys.argv[2].upper()
     prenom=sys.argv[3].capitalize()
     notes=sys.argv[4:]
     try:
-
+        total=0.0
+        for note in notes:
+            total+=float(note)
+        moyenne=total/len(notes)
+    except ValueError:
+        print("Erraur: Veuillez entrer des notes valides sous forme de nombres.")
+        exit(1)
+    try:
+        with open(f_path, mode='a', newline="") as fichier_csv:
+            writer=csv.writer(fichier_csv)
+            writer.writerow([nom, prenom, round(moyenne,1)])
+        print(f"Les notes de {prenom}{nom} ont été enregistrées dans {str(fichier)}")
+    except Exception as e:
+        print(f"Erreur:{e}")
